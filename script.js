e17020534122781037084b19fb42ee63afd33736
vv20 <vv20@st-andrews.ac.uk>
2019-04-18 23:25:22 +0100

    fixed axis

--- script.js
+++ script.js
@@ -2,26 +2,31 @@ var dataPath = "economic_freedom_index2019_data.csv";
 var correlationPath = "pearson_matrix.csv";
 
 // correlation matrix constants
-const zeroColour1 = 250
-const minColour1 = 245;
-const maxColour1 = 1
-const zeroColour2 = 250
-const minColour2 = 6;
-const maxColour2 = 78;
-const zeroColour3 = 5
-const minColour3 = 6;
-const maxColour3 = 12;
-const squareSide = 20;
+const zeroColour = "fafa05";
+const zeroColour1 = parseInt(zeroColour.substring(0,2), 16);
+const zeroColour2 = parseInt(zeroColour.substring(2,4), 16);
+const zeroColour3 = parseInt(zeroColour.substring(4,6), 16);
+const maxColour = "014e0c";
+const maxColour1 = parseInt(maxColour.substring(0,2), 16);
+const maxColour2 = parseInt(maxColour.substring(2,4), 16);
+const maxColour3 = parseInt(maxColour.substring(4,6), 16);
+const minColour = "f50606"
+const minColour1 = parseInt(minColour.substring(0,2), 16);
+const minColour2 = parseInt(minColour.substring(2,4), 16);
+const minColour3 = parseInt(minColour.substring(4,6), 16);
+const squareSide = 40;
 const correlationMatrixMargin = {
-    top: 10,
-    left: 10,
+    top: 70,
+    left: 150,
     right: 10,
     bottom: 10
 }
 const correlationMatrixWidth = 
-    750 - correlationMatrixMargin.left - correlationMatrixMargin.right;
+    1000 - correlationMatrixMargin.left - correlationMatrixMargin.right;
 const correlationMatrixHeight = 
-    750 - correlationMatrixMargin.top - correlationMatrixMargin.bottom;
+    12 * squareSide;
+
+// colour bar constants
 
 function getHTMLColour(min, max, value) {
     if (value < 0) {
@@ -59,12 +64,12 @@ function fillCorrelationMatrix(data) {
 
     x_range = []
     for (var i = 0; i < x_values.length; i++) {
-        x_range.push(squareSide * i);
+        x_range.push(squareSide * i + squareSide / 2);
     }
 
     y_range = []
     for (var i = 0; i < y_values.length; i++) {
-        y_range.push(squareSide * i);
+        y_range.push(squareSide * i + squareSide / 2);
     }
 
     x_scale = d3.scaleOrdinal().domain(x_values)
@@ -92,12 +97,25 @@ function fillCorrelationMatrix(data) {
     svg.selectAll("rect")
         .data(data).enter()
         .append("g").append("rect")
-        .attr("width", squareSide).attr("height", squareSide)
-        .attr("x", (d) => {return x_scale(d.x_name)})
-        .attr("y", (d) => {return y_scale(d.y_name)})
+        .attr("width", squareSide)
+        .attr("height", squareSide)
+        .attr("x", (d) => {return x_scale(d.x_name) - squareSide / 2})
+        .attr("y", (d) => {return y_scale(d.y_name) - squareSide / 2})
         .attr("fill", (d) => {
             return getHTMLColour(minValue, maxValue, d.correlation)
         });
+
+    svg.append("g").call(y_axis);
+    svg.append("g").call(x_axis)
+        .attr("transform", "translate(0," + correlationMatrixHeight + ")")
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("transform", (d) => {return "rotate(-45)"})
+}
+
+function fillColourBar() {
+    svg = d3.select("#colourBar")
+        .append("svg").attr("width")
 }
 
 d3.csv(dataPath).then(function(data) {
@@ -105,4 +123,5 @@ d3.csv(dataPath).then(function(data) {
 
 d3.csv(correlationPath).then((data) => {
     fillCorrelationMatrix(data);
+    fillColourBar();
 });
